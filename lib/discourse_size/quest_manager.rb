6e17d64120767f06bd2b2bf6c07132cfb5e7a2d2
Mid <mrrmiddynight@gmail.com>
2026-08-26 13:45:26 +0200

    Update quest_manager.rb

--- lib/discourse_size/quest_manager.rb
+++ lib/discourse_size/quest_manager.rb
@@ -8,7 +8,7 @@ module ::DiscourseSize
       { id: "topic_created_content", type: :topic_created, category_group: :content, min: 1, max: 1, reward: 15, emoji: "🎨" },
       { id: "post_created", type: :post_created, min: 1, max: 1, reward: 15, emoji: "✍️" },
       { id: "post_created_conv", type: :post_created, category_group: :conversation, min: 1, max: 1, reward: 15, emoji: "🗣️" },
-      { id: "post_created_content", type: :post_created, category_group: :content, min: 1, max: 2, reward: 15, emoji: "🖼️" },
+      { id: "post_created_content", type: :post_created, category_group: :content, min: 1, max: 1, reward: 15, emoji: "🖼️" },
       { id: "chat_message_created", type: :chat_message_created, min: 1, max: 10, reward: 5, emoji: "📱" },
       { id: "character_grow", type: :character_grow, min: 1, max: 1, reward: 15, emoji: "📈" },
       { id: "character_shrink", type: :character_shrink, min: 1, max: 1, reward: 15, emoji: "📉" },
