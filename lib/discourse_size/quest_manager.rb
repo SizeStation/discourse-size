38e92048eea71b48e69545a549645d7aca63251c
Mid <mrrmiddynight@gmail.com>
2026-09-18 23:17:16 +0200

    feat: tweak quest balancing

--- lib/discourse_size/quest_manager.rb
+++ lib/discourse_size/quest_manager.rb
@@ -51,10 +51,10 @@ module ::DiscourseSize
       },
       { id: "character_grow", type: :character_grow, min: 1, max: 1, reward: 15, emoji: "📈" },
       { id: "character_shrink", type: :character_shrink, min: 1, max: 1, reward: 15, emoji: "📉" },
-      { id: "like_created", type: :like_created, min: 2, max: 10, reward: 10, emoji: "❤️" },
+      { id: "like_created", type: :like_created, min: 2, max: 6, reward: 10, emoji: "❤️" },
       { id: "status_set", type: :status_set, min: 1, max: 1, reward: 5, emoji: "🟢" },
       { id: "post_read", type: :post_read, min: 3, max: 10, reward: 5, emoji: "👁️" },
-      { id: "item_gifted", type: :item_gifted, min: 1, max: 1, reward: 15, emoji: "🎁" },
+      { id: "item_gifted", type: :item_gifted, min: 1, max: 1, reward: 20, emoji: "🎁" },
     ].freeze
 
     MUTUALLY_EXCLUSIVE_TYPES = %i[topic_created post_created].freeze
